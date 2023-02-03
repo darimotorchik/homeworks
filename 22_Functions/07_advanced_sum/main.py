7902dce9d12b01b676a07a57de4175991110e2d4
darimotorchik <dasha_motorina@mail.ru>
2023-02-04 00:45:46 +0700

    задачи по 22 модулю

--- 22_Functions/07_advanced_sum/main.py
+++ 22_Functions/07_advanced_sum/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def my_sum(*obj):
+    str_obj = str(obj)
+    del_sym_obj = str_obj.translate({ord(i_sym): None for i_sym in '[],(){}'})
+    new_list = list(map(int, del_sym_obj.split()))
+    return sum(new_list)
+
+
+"""print(my_sum([[1, 2, [3]], [1], 3]))
+print(my_sum((1, 2, 3, 4, 5)))
+print(my_sum({1, 2, 3}))
+print(my_sum(1, 2, 3, 4, 5))"""
--- 22_Functions/08_list_of_lists_2/main.py
+++ 22_Functions/08_list_of_lists_2/main.py
@@ -1,4 +1,16 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
+result_list = []
 
-# TODO здесь писать код
+
+def unpack_list(some_list, res_list):
+    for i_elem in some_list:
+        if isinstance(i_elem, list):
+            unpack_list(i_elem, res_list)
+        else:
+            res_list.append(i_elem)
+    return res_list
+
+
+result = unpack_list(nice_list, result_list)
+print(f'Ответ: {result}')
